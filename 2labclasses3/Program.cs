c427c54719fbd7067ef76dc91caba9f1b157c7d1
massiveo1k <N.Synelnyk.FIT.125.20@knute.edu.ua>
2022-02-14 14:43:52 +0200

    Добавьте файлы проекта.

---
+++ 2labclasses3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2labclasses3
+{
+    internal class Program
+    {
+        public class User
+        {
+            public string someString = "Dodge1";
+            public string someString2 = "Mark";
+            public string someString3 = "Smith";
+            public int someInt = 20;
+            public DateTime someDateTime = DateTime.Now;    
+        }
+        static void Main(string[] args)
+        {
+            User someUser = new User();
+            Console.WriteLine($"Login: { someUser.someString}; Name: {someUser.someString2}; Surname: {someUser.someString3}; Age: {someUser.someInt}; date of filling in the questionnaire: {someUser.someDateTime};");
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ 2labclasses3/User.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2labclasses3
+{
+
+    internal class User
+    {
+        private string login;
+        private string name;
+        private string surname;
+        private int age;
+
+        public readonly DateTime date;
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                login = value;
+            }
+
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+
+
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+
+
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+
+
+        }
+
+        public User()
+        {
+            date = DateTime.Now;
+        }
+    }
+}
+
+
